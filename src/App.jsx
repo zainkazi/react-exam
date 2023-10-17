58ea5f6d7c9c5f307b3b2beabebfea1bf1bedcad
zain <zainkazi27@gmail.com>
2023-10-17 11:06:34 +0500

    implement dropdown filter for City and Occupation, and also age filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import sampleData from "./data/data";
 function App() {
   const [data, setData] = useState(sampleData);
   const [nameValue, setNameValue] = useState("");
-  const [ageValue, setAgeValue] = useState(0);
+  const [ageValue, setAgeValue] = useState("");
+  const [cityValue, setCityValue] = useState("");
+  const [occupationValue, setOccupationValue] = useState("");
   const [filteredData, setFilteredData] = useState(data);
 
   // useEffect(() => {
@@ -14,7 +16,13 @@ function App() {
 
   const filterData = () => {
     setFilteredData(
-      data.filter((p) => p.name.toLowerCase().includes(nameValue.toLowerCase()))
+      data.filter(
+        (p) =>
+          p.name.toLowerCase().includes(nameValue.toLowerCase()) &&
+          p.age.toString().includes(ageValue) &&
+          p.city.includes(cityValue) &&
+          p.occupation.includes(occupationValue)
+      )
     );
   };
 
@@ -24,6 +32,14 @@ function App() {
 
   const filterByAge = (e) => {
     setAgeValue(e.target.value);
+  };
+
+  const filterByCity = (e) => {
+    setCityValue(e.target.value);
+  };
+
+  const filterByOccupation = (e) => {
+    setOccupationValue(e.target.value);
   };
 
   const resetData = () => {};
@@ -36,7 +52,37 @@ function App() {
       <input type="text" onChange={filterByName} value={nameValue} />
 
       <label>Age</label>
-      <input type="number" onChange={filterByAge} value={ageValue} />
+      <input type="text" onChange={filterByAge} value={ageValue} />
+
+      <label>Select a City</label>
+      <select name="city" onChange={filterByCity}>
+        <option value=""></option>
+        <option value="New York">New York</option>
+        <option value="San Francisco">San Fransisco</option>
+        <option value="Chicago">Chicage</option>
+        <option value="Los Angeles">Los Angeles</option>
+        <option value="Houston">Houston</option>
+        <option value="Miami">Miami</option>
+        <option value="Seattle">Seattle</option>
+        <option value="Boston">Boston</option>
+        <option value="Denver">Denver</option>
+        <option value="Atlanta">Atlanta</option>
+      </select>
+
+      <label>Select Occupation</label>
+      <select name="occupation" onChange={filterByOccupation}>
+        <option value=""></option>
+        <option value="Engineer">Engineer</option>
+        <option value="Designer">Designer</option>
+        <option value="Accountant">Accountant</option>
+        <option value="Teacher">Teacher</option>
+        <option value="Doctor">Doctor</option>
+        <option value="Artist">Artist</option>
+        <option value="Softwar Eengineer">Software Engineer</option>
+        <option value="Lawyer">Lawyer</option>
+        <option value="Marketing Manager">Marketing Manager</option>
+        <option value="Entrepreneur">Entrepreneur</option>
+      </select>
 
       <button onClick={filterData}>Search</button>
 
